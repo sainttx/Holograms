55635dd3a22775b55282d2cae381bf1588438555
Matthew <matthew.steglinski@utoronto.ca>
2016-03-04 14:53:11 -0500

    Add missing documentation

--- Holograms-API/src/main/java/com/sainttx/holograms/api/entity/MountedEntity.java
+++ Holograms-API/src/main/java/com/sainttx/holograms/api/entity/MountedEntity.java
@@ -2,7 +2,18 @@ package com.sainttx.holograms.api.entity;
 
 public interface MountedEntity extends HologramEntity {
 
+    /**
+     * Returns the current mount that this entity is sitting on.
+     *
+     * @return the mount
+     */
     HologramEntity getMount();
 
+    /**
+     * Mounts this item to a new entity. Passing in a null value
+     * will remove any mount that the item is currently sitting on.
+     *
+     * @param entity new mount
+     */
     void setMount(HologramEntity entity);
 }
