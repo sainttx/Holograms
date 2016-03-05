297f61b7c2e26dfbbc7cf47acef169970a64e142
Matthew <matthew.steglinski@utoronto.ca>
2016-03-05 16:08:23 -0500

    Revert temporary ItemLine addition in CommandAddLine

--- Holograms/src/main/java/com/sainttx/holograms/commands/CommandAddLine.java
+++ Holograms/src/main/java/com/sainttx/holograms/commands/CommandAddLine.java
@@ -1,16 +1,14 @@
 package com.sainttx.holograms.commands;
 
 import com.sainttx.holograms.api.Hologram;
-import com.sainttx.holograms.api.HologramPlugin;
 import com.sainttx.holograms.api.line.HologramLine;
-import com.sainttx.holograms.api.line.ItemLine;
+import com.sainttx.holograms.api.HologramPlugin;
+import com.sainttx.holograms.api.line.TextLine;
 import com.sainttx.holograms.util.TextUtil;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.inventory.ItemStack;
 
 public class CommandAddLine implements CommandExecutor {
 
@@ -32,7 +30,7 @@ public class CommandAddLine implements CommandExecutor {
                 sender.sendMessage(ChatColor.RED + "Couldn't find a hologram with name \"" + hologramName + "\".");
             } else {
                 String text = TextUtil.implode(2, args);
-                HologramLine line = new ItemLine(hologram, new ItemStack(Material.ENDER_PEARL));
+                HologramLine line = new TextLine(hologram, text);
                 hologram.addLine(line);
                 sender.sendMessage(TextUtil.color("&7Added line &f\"" + text + "\" &7to hologram &f\"" + hologram.getId() + "&f\"."));
             }
